11a613d4439330f2589dd72c4f8c2b8f78e0964a
Dennis Loose <dennis.loose@canonical.com>
2023-01-24 17:02:02 +0100

    connectivity_plus_linux: update tests

--- packages/connectivity_plus/connectivity_plus/test/connectivity_plus_linux_test.dart
+++ packages/connectivity_plus/connectivity_plus/test/connectivity_plus_linux_test.dart
@@ -3,104 +3,233 @@ import 'package:connectivity_plus_platform_interface/connectivity_plus_platform_
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:networkd/networkd.dart';
 import 'package:nm/nm.dart';
 
 import 'connectivity_plus_linux_test.mocks.dart';
 
-@GenerateMocks([NetworkManagerClient])
+@GenerateMocks([NetworkManagerClient, NetworkdManager])
 void main() {
   test('registered instance', () {
     ConnectivityPlusLinuxPlugin.registerWith();
     expect(ConnectivityPlatform.instance, isA<ConnectivityPlusLinuxPlugin>());
   });
 
-  test('bluetooth', () async {
-    final linux = ConnectivityPlusLinuxPlugin();
-    linux.createClient = () {
-      final client = MockNetworkManagerClient();
-      when(client.connectivity)
-          .thenReturn(NetworkManagerConnectivityState.full);
-      when(client.primaryConnectionType).thenReturn('bluetooth');
-      return client;
-    };
-    expect(
-      linux.checkConnectivity(),
-      completion(equals(ConnectivityResult.bluetooth)),
-    );
+  group('NetworkManager connections', () {
+    test('bluetooth', () async {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connectivity)
+            .thenReturn(NetworkManagerConnectivityState.full);
+        when(client.primaryConnectionType).thenReturn('bluetooth');
+        return client;
+      };
+      linux.createNetworkdManager = () => MockNetworkdManager();
+      expect(
+        linux.checkConnectivity(),
+        completion(equals(ConnectivityResult.bluetooth)),
+      );
+    });
+
+    test('ethernet', () async {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connectivity)
+            .thenReturn(NetworkManagerConnectivityState.full);
+        when(client.primaryConnectionType).thenReturn('ethernet');
+        return client;
+      };
+      linux.createNetworkdManager = () => MockNetworkdManager();
+      expect(
+        linux.checkConnectivity(),
+        completion(equals(ConnectivityResult.ethernet)),
+      );
+    });
+
+    test('wireless', () async {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connectivity)
+            .thenReturn(NetworkManagerConnectivityState.full);
+        when(client.primaryConnectionType).thenReturn('wireless');
+        return client;
+      };
+      linux.createNetworkdManager = () => MockNetworkdManager();
+      expect(
+        linux.checkConnectivity(),
+        completion(equals(ConnectivityResult.wifi)),
+      );
+    });
+
+    test('vpn', () async {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connectivity)
+            .thenReturn(NetworkManagerConnectivityState.full);
+        when(client.primaryConnectionType).thenReturn('vpn');
+        return client;
+      };
+      linux.createNetworkdManager = () => MockNetworkdManager();
+      expect(
+        linux.checkConnectivity(),
+        completion(equals(ConnectivityResult.vpn)),
+      );
+    });
+
+    test('connectivity changes', () {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connectivity)
+            .thenReturn(NetworkManagerConnectivityState.full);
+        when(client.primaryConnectionType).thenReturn('wireless');
+        when(client.propertiesChanged).thenAnswer((_) {
+          when(client.connectivity)
+              .thenReturn(NetworkManagerConnectivityState.none);
+          return Stream.value(['Connectivity']);
+        });
+        return client;
+      };
+      linux.createNetworkdManager = () {
+        final networkdManager = MockNetworkdManager();
+        when(networkdManager.onlineState).thenReturn('');
+        when(networkdManager.propertiesChanged)
+            .thenAnswer((_) => const Stream.empty());
+        when(networkdManager.describe())
+            .thenAnswer((_) async => NetworkdManagerDescription());
+        return networkdManager;
+      };
+      expect(linux.onConnectivityChanged,
+          emitsInOrder([ConnectivityResult.wifi, ConnectivityResult.none]));
+    });
   });
 
-  test('ethernet', () async {
-    final linux = ConnectivityPlusLinuxPlugin();
-    linux.createClient = () {
-      final client = MockNetworkManagerClient();
-      when(client.connectivity)
-          .thenReturn(NetworkManagerConnectivityState.full);
-      when(client.primaryConnectionType).thenReturn('ethernet');
-      return client;
-    };
-    expect(
-      linux.checkConnectivity(),
-      completion(equals(ConnectivityResult.ethernet)),
-    );
+  group('systemd-networkd connections', () {
+    test('ethernet', () async {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connect()).thenThrow(Exception('error'));
+        return client;
+      };
+      linux.createNetworkdManager = () {
+        final networkdManager = MockNetworkdManager();
+        when(networkdManager.onlineState).thenReturn('online');
+        when(networkdManager.describe()).thenAnswer((_) async =>
+            NetworkdManagerDescription(interfaces: [
+              NetworkdLinkDescription(requiredForOnline: true, type: 'ether')
+            ]));
+        return networkdManager;
+      };
+      expect(
+        linux.checkConnectivity(),
+        completion(equals(ConnectivityResult.ethernet)),
+      );
+    });
+    test('wifi', () async {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connect()).thenThrow(Exception('error'));
+        return client;
+      };
+      linux.createNetworkdManager = () {
+        final networkdManager = MockNetworkdManager();
+        when(networkdManager.onlineState).thenReturn('online');
+        when(networkdManager.describe()).thenAnswer((_) async =>
+            NetworkdManagerDescription(interfaces: [
+              NetworkdLinkDescription(requiredForOnline: true, type: 'wlan')
+            ]));
+        return networkdManager;
+      };
+      expect(
+        linux.checkConnectivity(),
+        completion(equals(ConnectivityResult.wifi)),
+      );
+    });
+    test('mobile', () async {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connect()).thenThrow(Exception('error'));
+        return client;
+      };
+      linux.createNetworkdManager = () {
+        final networkdManager = MockNetworkdManager();
+        when(networkdManager.onlineState).thenReturn('online');
+        when(networkdManager.describe()).thenAnswer((_) async =>
+            NetworkdManagerDescription(interfaces: [
+              NetworkdLinkDescription(requiredForOnline: true, type: 'wwan')
+            ]));
+        return networkdManager;
+      };
+      expect(
+        linux.checkConnectivity(),
+        completion(equals(ConnectivityResult.mobile)),
+      );
+    });
+
+    test('connectivity changes', () {
+      final linux = ConnectivityPlusLinuxPlugin();
+      linux.createNetworkManagerClient = () {
+        final client = MockNetworkManagerClient();
+        when(client.connect()).thenThrow(Exception('error'));
+        return client;
+      };
+      linux.createNetworkdManager = () {
+        final networkdManager = MockNetworkdManager();
+        when(networkdManager.onlineState).thenReturn('online');
+        when(networkdManager.propertiesChanged).thenAnswer((_) {
+          when(networkdManager.onlineState).thenReturn('');
+          return Stream.value(['OnlineState']);
+        });
+        when(networkdManager.describe()).thenAnswer((_) async =>
+            NetworkdManagerDescription(interfaces: [
+              NetworkdLinkDescription(requiredForOnline: true, type: 'wlan')
+            ]));
+        return networkdManager;
+      };
+      expect(linux.onConnectivityChanged,
+          emitsInOrder([ConnectivityResult.wifi, ConnectivityResult.none]));
+    });
   });
 
-  test('wireless', () async {
+  test('no connectivity with fallback', () async {
     final linux = ConnectivityPlusLinuxPlugin();
-    linux.createClient = () {
-      final client = MockNetworkManagerClient();
-      when(client.connectivity)
-          .thenReturn(NetworkManagerConnectivityState.full);
-      when(client.primaryConnectionType).thenReturn('wireless');
-      return client;
-    };
-    expect(
-      linux.checkConnectivity(),
-      completion(equals(ConnectivityResult.wifi)),
-    );
-  });
-
-  test('vpn', () async {
-    final linux = ConnectivityPlusLinuxPlugin();
-    linux.createClient = () {
-      final client = MockNetworkManagerClient();
-      when(client.connectivity)
-          .thenReturn(NetworkManagerConnectivityState.full);
-      when(client.primaryConnectionType).thenReturn('vpn');
-      return client;
-    };
-    expect(
-      linux.checkConnectivity(),
-      completion(equals(ConnectivityResult.vpn)),
-    );
-  });
-
-  test('no connectivity', () async {
-    final linux = ConnectivityPlusLinuxPlugin();
-    linux.createClient = () {
+    linux.createNetworkManagerClient = () {
       final client = MockNetworkManagerClient();
       when(client.connectivity)
           .thenReturn(NetworkManagerConnectivityState.none);
       return client;
     };
+    linux.createNetworkdManager = () {
+      final networkdManager = MockNetworkdManager();
+      when(networkdManager.onlineState).thenReturn('');
+      when(networkdManager.describe())
+          .thenAnswer((_) async => NetworkdManagerDescription());
+      return networkdManager;
+    };
     expect(
         linux.checkConnectivity(), completion(equals(ConnectivityResult.none)));
   });
 
-  test('connectivity changes', () {
+  test('no services available', () async {
     final linux = ConnectivityPlusLinuxPlugin();
-    linux.createClient = () {
+    linux.createNetworkManagerClient = () {
       final client = MockNetworkManagerClient();
-      when(client.connectivity)
-          .thenReturn(NetworkManagerConnectivityState.full);
-      when(client.primaryConnectionType).thenReturn('wireless');
-      when(client.propertiesChanged).thenAnswer((_) {
-        when(client.connectivity)
-            .thenReturn(NetworkManagerConnectivityState.none);
-        return Stream.value(['Connectivity']);
-      });
+      when(client.connect()).thenThrow(Exception('error'));
       return client;
     };
-    expect(linux.onConnectivityChanged,
-        emitsInOrder([ConnectivityResult.wifi, ConnectivityResult.none]));
+    linux.createNetworkdManager = () {
+      final networkdManager = MockNetworkdManager();
+      when(networkdManager.connect()).thenThrow(Exception('error'));
+      return networkdManager;
+    };
+    expect(
+        linux.checkConnectivity(), completion(equals(ConnectivityResult.none)));
   });
 }
