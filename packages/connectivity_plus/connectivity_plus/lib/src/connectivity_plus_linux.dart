217567258f138dbbb317ec1ad3569010f65f1fc8
Dennis Loose <dennis.loose@canonical.com>
2023-01-24 16:58:39 +0100

    connectivity_plus_linux: use networkd as fallback

--- packages/connectivity_plus/connectivity_plus/lib/src/connectivity_plus_linux.dart
+++ packages/connectivity_plus/connectivity_plus/lib/src/connectivity_plus_linux.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:connectivity_plus_platform_interface/connectivity_plus_platform_interface.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
+import 'package:networkd/networkd.dart';
 import 'package:nm/nm.dart';
 
 // Used internally
@@ -9,6 +10,9 @@ import 'package:nm/nm.dart';
 
 @visibleForTesting
 typedef NetworkManagerClientFactory = NetworkManagerClient Function();
+
+@visibleForTesting
+typedef NetworkdManagerFactory = NetworkdManager Function();
 
 /// The Linux implementation of ConnectivityPlatform.
 class ConnectivityPlusLinuxPlugin extends ConnectivityPlatform {
@@ -20,14 +24,31 @@ class ConnectivityPlusLinuxPlugin extends ConnectivityPlatform {
   /// Checks the connection status of the device.
   @override
   Future<ConnectivityResult> checkConnectivity() async {
-    final client = createClient();
-    await client.connect();
-    final connectivity = _getConnectivity(client);
-    await client.close();
-    return connectivity;
+    NetworkManagerClient? networkManagerClient = createNetworkManagerClient();
+    NetworkdManager? networkdManager = createNetworkdManager();
+
+    try {
+      await networkManagerClient.connect();
+    } on Exception catch (e) {
+      debugPrint('NetworkManager: cannot connect to DBus API ($e)');
+      networkManagerClient = null;
+    }
+
+    try {
+      await networkdManager.connect();
+    } on Exception catch (e) {
+      debugPrint('systemd-networkd: cannot connect to DBus API ($e)');
+      networkdManager = null;
+    }
+
+    return _getConnectivity(
+      networkManagerClient: networkManagerClient,
+      networkdManager: networkdManager,
+    );
   }
 
-  NetworkManagerClient? _client;
+  NetworkManagerClient? _networkManagerClient;
+  NetworkdManager? _networkdManager;
   StreamController<ConnectivityResult>? _controller;
 
   /// Returns a Stream of ConnectivityResults changes.
@@ -40,7 +61,8 @@ class ConnectivityPlusLinuxPlugin extends ConnectivityPlatform {
     return _controller!.stream;
   }
 
-  ConnectivityResult _getConnectivity(NetworkManagerClient client) {
+  ConnectivityResult _getNetworkManagerConnectivity(
+      NetworkManagerClient client) {
     if (client.connectivity != NetworkManagerConnectivityState.full) {
       return ConnectivityResult.none;
     }
@@ -59,27 +81,99 @@ class ConnectivityPlusLinuxPlugin extends ConnectivityPlatform {
     return ConnectivityResult.mobile;
   }
 
+  Future<ConnectivityResult> _getNetworkdConnectivity(
+      NetworkdManager networkdManager) async {
+    final desc = await networkdManager.describe();
+    NetworkdLinkDescription? linkDescription;
+    for (final link in desc.interfaces ?? <NetworkdLinkDescription>[]) {
+      if (link.requiredForOnline ?? false) {
+        linkDescription = link;
+      }
+    }
+    if (networkdManager.onlineState != 'online' || linkDescription == null) {
+      return ConnectivityResult.none;
+    }
+    if (linkDescription.type?.contains('ether') ?? false) {
+      return ConnectivityResult.ethernet;
+    }
+    if (linkDescription.type?.contains('wlan') ?? false) {
+      return ConnectivityResult.wifi;
+    }
+    if (linkDescription.type?.contains('wwan') ?? false) {
+      return ConnectivityResult.mobile;
+    }
+    return ConnectivityResult.ethernet;
+  }
+
+  Future<ConnectivityResult> _getConnectivity({
+    NetworkManagerClient? networkManagerClient,
+    NetworkdManager? networkdManager,
+  }) async {
+    var connectivity = ConnectivityResult.none;
+    if (networkManagerClient != null) {
+      connectivity = _getNetworkManagerConnectivity(networkManagerClient);
+    }
+    if (connectivity == ConnectivityResult.none && networkdManager != null) {
+      connectivity = await _getNetworkdConnectivity(networkdManager);
+    }
+    return connectivity;
+  }
+
   Future<void> _startListenConnectivity() async {
-    _client ??= createClient();
-    await _client!.connect();
-    _addConnectivity(_client!);
-    _client!.propertiesChanged.listen((properties) {
+    await _startListenNetworkManagerConnectivity().catchError((e) {
+      debugPrint('NetworkManager: cannot listen to connectivity changes ($e)');
+      _networkManagerClient = null;
+    });
+
+    await _startListenNetworkdConnectivity().catchError((e) {
+      debugPrint(
+          'systemd-networkd: cannot listen to connectivity changes ($e)');
+      _networkdManager = null;
+    });
+  }
+
+  Future<void> _addConnectivity() async =>
+      _controller!.add(await _getConnectivity(
+        networkManagerClient: _networkManagerClient,
+        networkdManager: _networkdManager,
+      ));
+
+  Future<void> _startListenNetworkManagerConnectivity() async {
+    _networkManagerClient ??= createNetworkManagerClient();
+    await _networkManagerClient!.connect();
+
+    _addConnectivity();
+    _networkManagerClient!.propertiesChanged.listen((properties) {
       if (properties.contains('Connectivity')) {
-        _addConnectivity(_client!);
+        _addConnectivity();
       }
     });
   }
 
-  void _addConnectivity(NetworkManagerClient client) {
-    _controller!.add(_getConnectivity(client));
+  Future<void> _startListenNetworkdConnectivity() async {
+    _networkdManager ??= createNetworkdManager();
+    await _networkdManager!.connect();
+    await _addConnectivity();
+    _networkdManager!.propertiesChanged.listen((properties) {
+      if (properties.contains('OnlineState')) {
+        _addConnectivity();
+      }
+    });
   }
 
   Future<void> _stopListenConnectivity() async {
-    await _client?.close();
-    _client = null;
+    await _networkManagerClient?.close();
+    _networkManagerClient = null;
+    await _networkdManager?.close();
+    _networkdManager = null;
   }
 
   @visibleForTesting
   // ignore: prefer_function_declarations_over_variables
-  NetworkManagerClientFactory createClient = () => NetworkManagerClient();
+  NetworkManagerClientFactory createNetworkManagerClient =
+      () => NetworkManagerClient();
+
+  @visibleForTesting
+  // ignore: prefer_function_declarations_over_variables
+  NetworkdManagerFactory createNetworkdManager = () => NetworkdManager();
 }
